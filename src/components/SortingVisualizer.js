1152e4adad3c240f3836f6f350692b9331560f55
NES <nspry1997@gmail.com>
2020-08-23 15:10:19 -0400

    removed options

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { resetArray } from '../utils/functions'
 import { mergeSort } from '../utils/mergeSort'
-import { quickSort } from '../utils/quickSort'
-import { heapSort } from '../utils/heapSort'
-import { bubbleSort } from '../utils/bubbleSort'
 import { mergeSortAnimation } from '../utils/mergeAnimation'
-import { quickSortAnimation } from '../utils/quickAnimation'
+
 
 export const SortingVisualizer = () => {
   const [array, setArray] = useState([])
@@ -34,9 +31,6 @@ export const SortingVisualizer = () => {
       <div className="btn__container">
         <button className="btn" onClick={() => setArray(resetArray(intArrayBars, arrayMin, arrayMax))}>Generate New Array</button>
         <button className="btn" onClick={() => mergeSort(mergeSortAnimation(array), animationSpeed, primaryColor, secondaryColor)}>Merge Sort</button>
-        <button className="btn" onClick={() => quickSort(quickSortAnimation(array), animationSpeed, primaryColor, secondaryColor)}>Quick Sort</button>
-        <button className="btn" onClick={() => heapSort()}>Heap Sort</button>
-        <button className="btn" onClick={() => bubbleSort()}>Bubble Sort</button>
       </div>
     </div>
   )
